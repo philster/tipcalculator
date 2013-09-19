57e9e5f3c0050b420da136b74158d4e04cc0f529
Phil Wee <cyber_peewee@yahoo.com>
2013-09-19 00:03:21 -0700

    initial commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Phil Wee on 9/18/13.
+//  Copyright (c) 2013 Philster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Phil Wee on 9/18/13.
+//  Copyright (c) 2013 Philster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
